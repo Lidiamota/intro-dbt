3fb7e33641a9eca5c6ff89499e05311b452bf038
Lidia do Carmo Sequeira da Mota <lidiacsmota@gmail.com>
2024-09-07 20:40:18 +0000

    Última versão da correção exercicio 3.2.2

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -11,12 +11,10 @@ renamed as (
     select
         orders_id,
         shipping_fee,
-        shipping_fee_1,
         logcost,
-        CAST(ship_cost AS FLOAT) AS ship_cost,
+        CAST(ship_cost AS FLOAT64) AS ship_cost,
 
     from source
-    WHERE shipping_fee <> shipping_fee_1
-)
+    )
 
 select * from renamed
