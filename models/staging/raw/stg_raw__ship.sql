8cebbed99fdcc4f779be83428e1b88f2c39c6207
Lidia do Carmo Sequeira da Mota <lidiacsmota@gmail.com>
2024-09-07 20:09:42 +0000

    Alterações solicitadas no exercício 3.2.2

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -11,9 +11,10 @@ renamed as (
     select
         orders_id,
         shipping_fee,
-        shipping_fee_1,
-        logcost AS log_cost,
-        ship_cost
+        CASE WHEN shipping_fee <> shipping_fee_1 THEN shipping_fee ELSE NULL END AS shipping_fee_1,
+    ,
+        logcost,
+        CAST(ship_cost AS NUMERIC) AS ship_cost
 
     from source
 
